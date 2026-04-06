4f3819314b3bec80e4a6aa1c3751162be81fad27
Hector <hector.pasantes.7e9@itb.cat>
2026-04-06 22:48:33 +0200

    Added card class and deck class

---
+++ Core/Models/Card.cs
@@ -0,0 +1,42 @@
+﻿using Poker.Core.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker.Core.Models
+{
+    public class Card
+    {
+        public Symbols Symbol { get; set; }
+        public int Num { get; set; }
+        public Card(Symbols symbol, int num)
+        {
+            Symbol = symbol;
+            Num = num;
+        }
+
+        public static Symbols GetTypeCard(int randSymbol)
+        {
+            Symbols symbol = Symbols.Heart;
+
+            switch (randSymbol)
+            {
+                case 1:
+                    symbol = Symbols.Heart;
+                    break;
+
+                case 2:
+                    symbol = Symbols.Club;
+                    break;
+                case 3:
+                    symbol = Symbols.Diamond;
+                    break;
+                case 4:
+                    symbol = Symbols.Spade;
+                    break;
+            }
+
+            return symbol;
+        }
+    }
+}
---
+++ Core/Models/Enum/Symbols.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker.Core.Models.Enum
+{
+    public enum Symbols
+    {
+        Heart = 1,
+        Club = 2,
+        Diamond = 3,
+        Spade = 4
+    }
+}
